1bbdb715f23fded2f6281bb341d5ded1f2f988cd
lenardsilvester <72597718+lenardsilvester@users.noreply.github.com>
2021-05-31 11:42:48 +0200

    some progress

--- src/includes/layouts/footer.php
+++ src/includes/layouts/footer.php
@@ -1,10 +1,12 @@
 <footer>
   <p>&copy; dev-forum <b><?php echo date('Y'); ?></b>.</p>
   <div>
-    Socials &nbsp; -&nbsp;
-    <a href="#"><i class="bi bi-twitter"></i></a>
-    <a href="#"><i class="bi bi-instagram"></i></a>
-    <a href="#"><i class="bi bi-youtube"></i></a>
+    <p>
+      Socials &nbsp; -&nbsp;
+      <a href="#"><i class="bi bi-twitter"></i></a>
+      <a href="#"><i class="bi bi-instagram"></i></a>
+      <a href="#"><i class="bi bi-youtube"></i></a>
+    </p>
   </div>
 </footer>
 </body>
